1d662783ef28ada3c7d2c9ec8c11c9d3ce573437
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-03-09 18:26:46 +0000

    Pass tests

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 (function(mod) {
 
+    var _splitKeep = function (input, delimiter) {
+        var split = input.split(delimiter);
+
+        for (var i = 0; i < split.length - 1; i++) {
+            split[i] += delimiter;
+        }
+
+        return split;
+    };
+
     mod.exports = function (input) {
-        return '    ' + input;
+        var lines = _splitKeep(input, "\n");
+        var output = '';
+
+        for (var i = 0; i < lines.length; i++) {
+            output += '    ' + lines[i];
+        }
+
+        return output;
     };
 
 })(module);
--- test.js
+++ test.js
@@ -12,7 +12,7 @@ test('it indents single line by 4 spaces', function (t, input, expected) {
 test('it splits a string with 2 lines and indents each by 4 spaces', function (t, input, expected) {
     t.assert.equal(expected, indent(input));
 }, [
-    ['\\n','    \\n    '],
-    ['i am also\\na humble test case', '    i am also\\n    a humble test case'],
-    ['nothing is worse than\\nunindented text!', '    nothing is worse than\\n    unindented text!']
+    ['\n','    \n    '],
+    ['i am also\na humble test case', '    i am also\n    a humble test case'],
+    ['nothing is worse than\nunindented text!', '    nothing is worse than\n    unindented text!']
 ]);
