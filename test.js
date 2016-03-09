3e9e86b89bf67d1afe00b10252ba819bf72257ad
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-03-09 18:08:16 +0000

    Add second test

--- test.js
+++ test.js
@@ -10,3 +10,13 @@ test('it indents single line by 4 spaces', function (t, input, expected) {
     ['a great philosopher once said', '    a great philosopher once said'],
     ['build that wall!', '    build that wall!']
 ]);
+
+test('it splits a string with 2 lines and indents each by 4 spaces', function (t, input, expected) {
+    var output = indent(input);
+
+    t.assert.equal(expected, indent(input));
+}, [
+    ['\\n','    \\n    '],
+    ['i am also\\na humble test case', '    i am also\\n    a humble test case'],
+    ['nothing is worse than\\nunindented text!', '    nothing is worse than\\n    unindented text!']
+]);
