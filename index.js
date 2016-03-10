7ec5bd8c3556907f76e14524bd665290c5979dbd
James Monger <jameskmonger@hotmail.co.uk>
2016-03-10 00:27:18 +0000

    Use external splitkeep module

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 (function(mod) {
 
-    var _splitKeep = function (input, delimiter) {
-        var split = input.split(delimiter);
-
-        for (var i = 0; i < split.length - 1; i++) {
-            split[i] += delimiter;
-        }
-
-        return split;
-    };
+    var _splitKeep = require('splitkeep');
 
     mod.exports = function (input) {
         var lines = _splitKeep(input, "\n");
