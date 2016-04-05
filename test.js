2896174643462dccbbce34fc281eb63a551c9f08
James Monger <jameskmonger@hotmail.co.uk>
2016-04-06 00:11:58 +0100

    Test for 4 spaces specified

--- test.js
+++ test.js
@@ -33,3 +33,36 @@ test('it splits a string with 3 lines and indents each by 4 spaces if no space c
     ['a\nb\nc', '    a\n    b\n    c'],
     ['james\nmonger\ngithub', '    james\n    monger\n    github']
 ]);
+
+test('it indents single line by the specified space count', function (t, input, expected, count) {
+    var safeExpected = expected.replace('\n', '\\n');
+    var safeResult = indent(input, count).replace('\n', '\\n');
+
+    t.assert.equal(safeExpected, safeResult);
+}, [
+    ['', '    ', 4],
+    ['a great philosopher once said', '    a great philosopher once said', 4],
+    ['build that wall!', '    build that wall!', 4]
+]);
+
+test('it splits a string with 2 lines and indents each by the specified space count', function (t, input, expected, count) {
+    var safeExpected = expected.replace('\n', '\\n');
+    var safeResult = indent(input, count).replace('\n', '\\n');
+
+    t.assert.equal(safeExpected, safeResult);
+}, [
+    ['\n','    \n    ', 4],
+    ['i am also\na humble test case', '    i am also\n    a humble test case', 4],
+    ['nothing is worse than\nunindented text!', '    nothing is worse than\n    unindented text!', 4]
+]);
+
+test('it splits a string with 3 lines and indents each by the specified space count', function (t, input, expected, count) {
+    var safeExpected = expected.replace('\n', '\\n');
+    var safeResult = indent(input, count).replace('\n', '\\n');
+
+    t.assert.equal(safeExpected, safeResult);
+}, [
+    ['\n\n','    \n    \n    ', 4],
+    ['a\nb\nc', '    a\n    b\n    c', 4],
+    ['james\nmonger\ngithub', '    james\n    monger\n    github', 4]
+]);
